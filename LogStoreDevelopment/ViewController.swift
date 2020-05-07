e39400a24659d7043415c619a81525e10ec0e007
Ned Hogan <nedhogan@me.com>
2020-05-07 10:36:43 -0700

    Init base package without Logger package dependency
    This is the base logger packager, to be shared on GitHub after completion.

---
+++ LogStoreDevelopment/ViewController.swift
@@ -0,0 +1,16 @@
+//  Created by dasdom on 27.10.19.
+//  Copyright © 2019 dasdom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
